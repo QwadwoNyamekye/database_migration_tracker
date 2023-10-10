1f2a9172c292f6474596c2311bf48bd6318abff7
Qwadwo Nyamekye <oppongnkrumahdavie@gmail.com>
2023-10-10 22:10:32 +0000

    added aatabase config

---
+++ internal/database/database.go
@@ -0,0 +1,25 @@
+package internal
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+var cfg = mysql.Config{
+	User: os.Getenv("DBUSER"),
+}
+
+func connect() {
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err!= nil{
+		log.Fatal(
+			err
+		)
+	}
+}
--- utilities/database/main.go
+++
@@ -1,4 +0,0 @@
-package database
-
-var credentials struct {
-}
